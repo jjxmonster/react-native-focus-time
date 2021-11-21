3bb6ec573b38e65ba117db24b625e9a9c0c66b6a
jjxmonster <kuba.tarabasz@wp.pl>
2021-11-21 16:26:57 +0100

    add progress bar to visalization time

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -8,7 +8,7 @@ import { colors } from '../utils/colors';
 const minutesToMillis = min => min * 1000 * 60;
 const formatTime = time => (time < 10 ? `0${time}` : time);
 
-export const Countdown = ({ minutes = 20, isPaused }) => {
+export const Countdown = ({ minutes = 20, isPaused, onProgress }) => {
    const [millis, setMillis] = useState(minutesToMillis(minutes));
 
    const interval = useRef(null);
@@ -19,7 +19,7 @@ export const Countdown = ({ minutes = 20, isPaused }) => {
             return time;
          }
          const timeLeft = time - 1000;
-
+         onProgress(timeLeft / minutesToMillis(minutes));
          return timeLeft;
       });
    };
